aa1d37d39d8769f49060a546834c5353ed6b94f6
veigasjeff <154071076+dsouzavero@users.noreply.github.com>
2024-01-30 21:14:17 +0530

    up

--- js/socialmedia.js
+++ js/socialmedia.js
@@ -42,70 +42,73 @@
 //   }
 
 
- // shareSocialMedia.json
-function shareSocialMedia(platform) {
-  const parentURL = window.location.href;
 
-  // Fetch movie data from movie.json
-  fetch('movie.json')
-    .then(response => response.json())
-    .then(movieData => {
-      // Get the id parameter from the URL
-      const urlParams = new URLSearchParams(window.location.search);
-      const idParam = urlParams.get('id');
+  // shareSocialMedia.js
+  function shareSocialMedia(platform) {
+    const parentURL = window.location.href;
 
-      // Find the movie with the matching id
-      const selectedMovie = movieData.find(movie => movie.id === idParam);
+    // Fetch movie title from <title> tag
+    const movieTitle = document.title;
 
-      if (selectedMovie) {
-        const movieName = selectedMovie.name;
-
-        switch (platform) {
-          case "facebook":
-            window.open(
-              `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-                parentURL
-              )}&quote=${encodeURIComponent(movieName)}`
-            );
-            break;
-          case "twitter":
-            window.open(
-              `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-                parentURL
-              )}&text=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
-                movieName
-              )}!`
-            );
-            break;
-          case "linkedin":
-            window.open(
-              `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(
-                parentURL
-              )}&title=${encodeURIComponent(movieName)}`
-            );
-            break;
-          case "whatsapp":
-            window.open(
-              `whatsapp://send?text=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
-                movieName
-              )}!%20${encodeURIComponent(parentURL)}`
-            );
-            break;
-          case "email":
-            window.location.href = `mailto:?subject=Check%20out%20${encodeURIComponent(
-              movieName
-            )}!&body=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
-              movieName
-            )}!%20${encodeURIComponent(parentURL)}`;
-            break;
-          default:
-            console.log("Unsupported platform.");
-        }
-      } else {
-        console.error('Movie not found');
+    // Fetch movie image from <meta> tag
+    const metaTags = document.head.getElementsByTagName('meta');
+    let movieImage;
+    let movieURL;
+    for (const tag of metaTags) {
+      if (tag.getAttribute('property') === 'og:image') {
+        movieImage = tag.getAttribute('content');
+      } else if (tag.getAttribute('property') === 'og:url') {
+        movieURL = tag.getAttribute('content');
       }
-    })
-    .catch(error => {
-      console.error('Error fetching movie data:', error);
-    });
-}
+    }
+
+    // If og:url is not present, fallback to the current URL
+    movieURL = movieURL || parentURL;
+
+    if (movieTitle && movieImage) {
+      switch (platform) {
+        case "facebook":
+          window.open(
+            `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+              movieURL
+            )}&quote=${encodeURIComponent(movieTitle)}`
+          );
+          break;
+        case "twitter":
+          window.open(
+            `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+              movieURL
+            )}&text=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
+              movieTitle
+            )}!`
+          );
+          break;
+        case "linkedin":
+          window.open(
+            `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(
+              movieURL
+            )}&title=${encodeURIComponent(movieTitle)}`
+          );
+          break;
+        case "whatsapp":
+          window.open(
+            `whatsapp://send?text=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
+              movieTitle
+            )}!%20${encodeURIComponent(movieURL)}`
+          );
+          break;
+        case "email":
+          window.location.href = `mailto:?subject=Check%20out%20${encodeURIComponent(
+            movieTitle
+          )}!&body=Check%20out%20this%20awesome%20movie%20${encodeURIComponent(
+            movieTitle
+          )}!%20${encodeURIComponent(movieURL)}`;
+          break;
+        default:
+          console.log("Unsupported platform.");
+      }
+    } else {
+      console.error('Movie title or image not found in the HTML.');
+    }
+  }
+
